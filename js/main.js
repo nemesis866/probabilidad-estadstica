f3c73c4091e38f85a71a403d5d58bcbbe19cd276
Paulo Andrade <source.compu@gmail.com>
2016-09-03 16:04:56 -0500

    Agregamos la ventana acerca de.

--- js/main.js
+++ js/main.js
@@ -95,7 +95,8 @@ const template = [
 			label: 'Acerca de',
 			click()
 			{
-				
+				// Ejecutamos la ventana acerca de
+				aboutWindow();
 			}
 		}
 	]
@@ -105,19 +106,20 @@ const template = [
 const menu = Menu.buildFromTemplate(template);
 Menu.setApplicationMenu(menu);
 
-
 // Mantener una referencia global del objeto window, si no lo hace, la ventana puede
 // cerrarse automaticamente cuando el objecto garbage collected se utilice.
-let win;
+let win, about;
 
 function createWindow ()
 {
 	// Creamos la ventana del navegador
 	win = new BrowserWindow({
+		backgroundColor: '#eee', // Fondo para crear efecto visual de aplicación nativa
 		height: 600, // Altura de la ventana
 		icon: 'favicon.ico', // Icono de la aplicacion
 		minHeight: 600, // Altura minima de la ventana
 		minWidth: 800, // Ancho minimo de la ventana
+		show: false, // Eliminamos el efecto de carga tipo web
 		title: 'Probabilidad',
 		width: 800 // Ancho de la ventana
 	});
@@ -138,6 +140,38 @@ function createWindow ()
 		// cierra la aplicacion
 		win = null;
 	});
+
+	// Mostramos la ventana cuando este lista
+	win.once('ready-to-show', () =>
+	{
+		// Mostramos la ventana
+		win.show();
+	});
+}
+
+function aboutWindow ()
+{
+	about = new BrowserWindow({
+		backgroundColor: '#eee', // Fondo de ventana
+		height: 350, // Alto de la ventana
+		icon: 'favicon.ico', // Icono de la aplicacion
+		parent: win, // Ventana padre
+		minimizable: false, // Indicamos si la ventana es minimizable
+		modal: true, // Tipo de vantana
+		resizable: false, // Indicamos si la ventana puede modificar su tamaño
+		show: false, // No mostrar
+		width: 750 // Ancho de la ventana
+	});
+
+	about.loadURL(`file://${__dirname}/../template/about.html`);
+
+	// Mostramos hasta que esten cargados todos los elementos
+	about.once('ready-to-show', () =>
+	{
+		about.show();
+	});
+
+	about.setMenu(null);
 }
 
 // Este método será llamado cuando Electron ha terminado
